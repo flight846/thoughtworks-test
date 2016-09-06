1c03b42ae2fd3047a12a852aeaebed8a70f76c5c
David Tan <davified@gmail.com>
2016-09-06 12:26:03 +0800

    refactored rates into objects

--- hotels.js
+++ hotels.js
@@ -1,18 +1,38 @@
 var parsingData = require('./parsing')
 
-function HotelOutlet (name, weekdayRate, loyaltyWeekdayRate, weekendRate, loyaltyWeekendRate, rating) {
+function Rates(weekdayRate, weekendRate) {
+  this.weekdayRate = weekdayRate
+  this.weekendRate = weekendRate
+
+  this.calculateRateFor = function(dayCount) {
+    var price = 0
+    price += dayCount.weekdays * this.weekdayRate
+    price += dayCount.weekends * this.weekendRate
+    return price
+  }
+}
+
+function HotelOutlet (name, rating, regularRate, rewardsRate) {
   this.name = name,
-  this.weekdayRate = weekdayRate,
-  this.loyaltyWeekdayRate = loyaltyWeekdayRate,
-  this.weekendRate = weekendRate,
-  this.loyaltyWeekendRate = loyaltyWeekendRate,
   this.rating = rating
+  this.regularRate = regularRate
+  this.rewardsRate = rewardsRate
+
+  this.calculatePrice = function(customerType, dayCount) {
+    var price = 0
+    if (customerType === 'Regular') {
+      price += this.regularRate.calculateRateFor(dayCount)
+    } else if (customerType === 'Rewards') {
+      price += this.rewardsRate.calculateRateFor(dayCount)
+    }
+    return price
+  }
 }
 
 var hotels = {
-  outlets: [new HotelOutlet('Lakewood', 110, 80, 90, 80, 3),
-    new HotelOutlet('Bridgewood', 160, 110, 60, 50, 4),
-    new HotelOutlet('Ridgewood', 220, 100, 150, 40, 5) ],
+  outlets: [new HotelOutlet('Lakewood', 3, new Rates(110, 90), new Rates(80, 80)),
+    new HotelOutlet('Bridgewood', 4, new Rates(160, 60), new Rates(110, 50)),
+    new HotelOutlet('Ridgewood', 5, new Rates(220, 150), new Rates(100, 40)) ],
 
   calculatePrices: (booking) => {
     var dayCount = parsingData.numberOfWeekdaysAndWeekends(booking)
@@ -20,16 +40,8 @@ var hotels = {
     var totalPrices = []
 
     hotels.outlets.forEach((element) => {
-      var price = 0
+      var price = element.calculatePrice(booking.customerType, dayCount)
       var obj = {}
-
-      if (booking.customerType === 'Regular') {
-        price += dayCount.weekdays * element.weekdayRate
-        price += dayCount.weekends * element.weekendRate
-      } else if (booking.customerType === 'Rewards') {
-        price += dayCount.weekdays * element.loyaltyWeekdayRate
-        price += dayCount.weekends * element.loyaltyWeekendRate
-      }
 
       obj['name'] = element.name
       obj['price'] = price
